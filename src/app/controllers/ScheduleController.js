025c6256b9c4313a43c909fbae31ac1e1a53d5e2
fidelis27 <thiago.fidelis27@gmail.com>
2019-12-12 11:05:21 -0300

    initial commit

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,34 @@
+import { Op } from 'sequelize';
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import Appointment from '../models/Appointments';
+import User from '../models/User';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ erro: 'User is not Provider' });
+    }
+
+    const { date } = req.query;
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  /* dsn: 'https://387014c1da474786b72318dde577403f@sentry.io/1829396' */
+  dsn: process.env.Sentry_DSN,
+};
