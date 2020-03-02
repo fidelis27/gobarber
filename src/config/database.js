9ff9f7b4b5323b872b1e6a666a0cdd3cadc61916
fidelis27 <thiago.fidelis27@gmail.com>
2020-03-02 16:05:05 -0300

    correção

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,20 @@
 require('dotenv/config');
+require('sequelize');
 
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
+  host: 'ec2-52-207-93-32.compute-1.amazonaws.com',
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
+  },
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true,
   },
 };
